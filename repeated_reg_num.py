6e33eb15b75d3f4ad3f7b8755345b323798e9cce
Vinodhini Parasuraman <vinonivi1@gmail.com>
2016-10-03 12:12:08 +0530

    Update repeated_reg_num.py

--- repeated_reg_num.py
+++ repeated_reg_num.py
@@ -10,4 +10,5 @@ def repeatedNumber(A):
     return rep_list
 
 if __name__ == '__main__':
-    print repeatedNumber([1,2,3,4,5,1,2,3])
+    reg_num_list = list(map(int,raw_input().split()))
+    print repeatedNumber(reg_num_list)
